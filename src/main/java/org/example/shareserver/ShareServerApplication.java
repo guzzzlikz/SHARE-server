df84b441dacc474ad6d3cb02f807289533af7be8
guzzzlikz <jaroslavvolk9@gmail.com>
2026-03-02 12:39:43 +0200

    Initial commit

---
+++ src/main/java/org/example/shareserver/ShareServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.shareserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/shareserver/ShareServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.shareserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
