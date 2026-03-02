acc38300bbd9b96d114494bdc7bbefca254e5388
guzzzlikz <jaroslavvolk9@gmail.com>
2026-03-02 13:56:31 +0200

    Configs, dependencies + .env reader

--- src/main/java/org/example/shareserver/ShareServerApplication.java
+++ src/main/java/org/example/shareserver/ShareServerApplication.java
@@ -1,12 +1,17 @@
 package org.example.shareserver;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class ShareServerApplication {
 
     public static void main(String[] args) {
+        Dotenv env = Dotenv.load();
+        System.setProperty("spring.mongodb.uri", Objects.requireNonNull(env.get("MONGO.URI")));
         SpringApplication.run(ShareServerApplication.class, args);
     }
 
