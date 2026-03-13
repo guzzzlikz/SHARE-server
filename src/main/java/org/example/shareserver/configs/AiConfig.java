1174974296e222fc769d0338646f558525ca78c3
guzzzlikz <jaroslavvolk9@gmail.com>
2026-03-13 23:58:09 +0200

    User AI profile

--- src/main/java/org/example/shareserver/configs/AiConfig.java
+++ src/main/java/org/example/shareserver/configs/AiConfig.java
@@ -3,6 +3,7 @@ package org.example.shareserver.configs;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -18,6 +19,14 @@ public class AiConfig {
                 .baseUrl("https://api.openai.com/v1")
                 .defaultHeader("Authorization", "Bearer " + AiKey)
                 .defaultHeader("Content-Type", "application/json")
+                .exchangeStrategies(
+                        ExchangeStrategies.builder()
+                                .codecs(configurer ->
+                                        configurer.defaultCodecs()
+                                                .maxInMemorySize(10 * 1024 * 1024)
+                                )
+                                .build()
+                )
                 .build();
     }
 }
--- src/main/java/org/example/shareserver/services/AiService.java
+++ src/main/java/org/example/shareserver/services/AiService.java
@@ -1,23 +1,40 @@
 package org.example.shareserver.services;
 
+import io.jsonwebtoken.security.Jwks;
+import lombok.extern.slf4j.Slf4j;
+import org.example.shareserver.models.dtos.OpenAIImageDTO;
 import org.example.shareserver.models.entities.Product;
 import org.example.shareserver.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.reactive.function.client.WebClient;
 import tools.jackson.databind.ObjectMapper;
+import org.springframework.mock.web.MockMultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Base64;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @Service
 @EnableCaching
 public class AiService {
     @Autowired
-    ObjectMapper objectMapper;
+    private PhotoStorageService photoStorageService;
+    @Autowired
+    private JWTService jwtService;
+    @Autowired
+    private ObjectMapper objectMapper;
     @Autowired
     private ProductRepository productRepository;
     @Autowired
@@ -57,6 +74,84 @@ public class AiService {
     public String giveDbToAi() {
         List<Product> products = productRepository.findAll();
         return objectMapper.writeValueAsString(products);
+    }
 
+    public ResponseEntity<?> generateProfile(MultipartFile file, String token) {
+        if (file == null || file.isEmpty()) {
+            log.info("AI service has been called but file is empty");
+            return ResponseEntity.status(400).body("File cannot be empty");
+        }
+        token = token.replace("Bearer ", "");
+        // Convert file to Base64
+        String base64File = null;
+        try {
+            base64File = Base64.getEncoder().encodeToString(file.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        MultiValueMap<String, Object> form = new LinkedMultiValueMap<>();
+
+        form.add("model", "gpt-image-1");
+        form.add("prompt", "convert this photo into anime style profile picture");
+        form.add("size", "1024x1024");
+
+        try {
+            form.add("image", new MultipartInputStreamFileResource(
+                    file.getInputStream(),
+                    file.getOriginalFilename()
+            ));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        log.info("Image added - sending request");
+
+        OpenAIImageDTO response = webClient.post()
+                .uri("/images/edits")
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .bodyValue(form)
+                .retrieve()
+                .bodyToMono(OpenAIImageDTO.class)
+                .block();
+        log.info("Request succeeded!");
+        String base64 = response.getData().get(0).getB64_json();
+        byte[] imageBytes = Base64.getDecoder().decode(base64);
+        String id = jwtService.getDataFromToken(token);
+        MultipartFile generatedFile = new MockMultipartFile(
+                "image",
+                "result.png",
+                "image/png",
+                imageBytes
+        );
+        String blob = null;
+        try {
+            blob = photoStorageService.uploadUserProfilePhoto(generatedFile, id);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if (blob == null) {
+            return ResponseEntity.status(401).build();
+        }
+        log.info("Blob succeeded!");
+        return ResponseEntity.ok().body(photoStorageService.getSignedUrl(blob));
+    }
+}
+class MultipartInputStreamFileResource extends InputStreamResource {
+
+    private final String filename;
+
+    public MultipartInputStreamFileResource(InputStream inputStream, String filename) {
+        super(inputStream);
+        this.filename = filename;
+    }
+
+    @Override
+    public String getFilename() {
+        return filename;
+    }
+
+    @Override
+    public long contentLength() {
+        return -1;
     }
 }
