1072c3c6e638573fe8d696c50504ca7709dec5ba
guzzzlikz <jaroslavvolk9@gmail.com>
2026-03-13 14:01:50 +0200

    GCS fixes

--- src/main/java/org/example/shareserver/ShareServerApplication.java
+++ src/main/java/org/example/shareserver/ShareServerApplication.java
@@ -8,14 +8,12 @@ import java.util.Objects;
 
 @SpringBootApplication
 public class ShareServerApplication {
-
     public static void main(String[] args) {
         Dotenv env = Dotenv.load();
         System.setProperty("spring.mongodb.uri", Objects.requireNonNull(env.get("MONGO.URI")));
         System.setProperty("jwt.secret", Objects.requireNonNull(env.get("JWT.SECRET")));
         System.setProperty("ai.key", Objects.requireNonNull(env.get("AI.KEY")));
-        System.setProperty("gcs.key", Objects.requireNonNull(env.get("GCS.KEY")));
+        System.setProperty("spring.cloud.gcp.credentials.json-key", Objects.requireNonNull(env.get("GCS.KEY")));
         SpringApplication.run(ShareServerApplication.class, args);
     }
-
 }
