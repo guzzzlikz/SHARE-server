02f4d268c3d24a8cd6f5681b293a327bcc2f08f4
guzzzlikz <jaroslavvolk9@gmail.com>
2026-03-13 14:01:50 +0200

    GCS fixes

--- src/main/java/org/example/shareserver/configs/GCSConfiguration.java
+++ src/main/java/org/example/shareserver/configs/GCSConfiguration.java
@@ -3,23 +3,30 @@ package org.example.shareserver.configs;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Base64;
 
 @Configuration
 public class GCSConfiguration {
-    public GCSConfiguration() throws IOException {
-        String json = System.getProperty("gcs.key");
-
-        GoogleCredentials credentials = GoogleCredentials.fromStream
-                (new ByteArrayInputStream(json.getBytes(StandardCharsets.UTF_8)));
-
+    @Bean
+    public Storage storage() throws IOException {
+        String json = System.getProperty("spring.cloud.gcp.credentials.json-key");
+        GoogleCredentials credentials = GoogleCredentials.fromStream(
+                new ByteArrayInputStream(json.getBytes(StandardCharsets.UTF_8)))
+                .createScoped(Arrays.asList("https://www.googleapis.com/auth/cloud-platform"));
         Storage storage = StorageOptions.newBuilder()
                 .setCredentials(credentials)
+                .setProjectId(System.getProperty("spring.cloud.gcp.projectId"))
                 .build()
                 .getService();
+        return storage;
     }
 }
